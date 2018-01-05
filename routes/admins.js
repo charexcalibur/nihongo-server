01c5a5db951a2e0bc8a9829c3e72ce4222aac03e
charexcalibur <charexcalibur@gmail.com>
2018-01-05 23:02:11 +0800

    fix 500error

--- routes/admins.js
+++ routes/admins.js
@@ -1,5 +1,6 @@
 'use strict'
-import dtime from 'time-formater'
+
+let dtime = require('time-formater')
 var express = require('express');
 var router = express.Router();
 const mongoose = require('mongoose')
@@ -9,6 +10,12 @@ const crypto = require('crypto')
 const bcrypt = require('bcryptjs')
 
 // id自增
+function getId() {
+  const idData = Ids.findOne()
+  idData ++
+  idData.save()
+  return idData
+}
 
 // 检查用户
 router.get('/checkuser', (req, res, next) => {
@@ -36,7 +43,8 @@ router.get('/checkuser', (req, res, next) => {
 router.post('/create', (req, res, next) => {
   let user_name = req.body.user_name
   let password = req.body.password
-
+  console.log(req.body)
+  let status = 1
   let salt = bcrypt.genSaltSync(10)
   let hash = bcrypt.hashSync(password, salt)
   console.log('hash: ' + hash)
@@ -44,7 +52,20 @@ router.post('/create', (req, res, next) => {
   Admins.create({
     user_name: user_name,
     password: hash,
-
+    create_time: dtime().format('YYYY-MM-DD HH:mm'),
+    status: status
+  }, (error, doc) => {
+    if (error) {
+      res.json({
+        status: '1',
+        msg: error.message
+      })
+    } else {
+      res.json({
+        status: '0',
+        msg: 'create suc'
+      })
+    }
   })
 
 
